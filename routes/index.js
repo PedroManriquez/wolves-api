4fe42169c4c60e27a52656a7ad4f5a9440e48d76
PedroManriquez <ptrmanriquez@gmail.com>
2018-01-19 14:07:28 -0300

    Prepared index.js file for support api routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const api = express();
+// Import api routes
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+const index = router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+api.use('/', index);
+/* API Router. */
+
+
+module.exports = api;

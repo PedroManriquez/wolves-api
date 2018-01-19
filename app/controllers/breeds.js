e612dd58abbafe1abb4fa1b5ee715e357de13ce2
PedroManriquez <ptrmanriquez@gmail.com>
2018-01-19 13:57:49 -0300

    Added breed controller

---
+++ app/controllers/breeds.js
@@ -0,0 +1,18 @@
+const breed = require('../models').breed;
+
+module.exports = {
+  create(request, response) {
+    return breed
+      .create({
+        title: request.body.name,
+      })
+      .then(data => response.status(201).send(data))
+      .catch(error => response.status(400).send(error));
+  },
+  index(request, response) {
+  	return breed
+  		.all()
+  		.then(data => response.status(200).send(data))
+  		.catch(err => response.status(400).send(err));
+  }
+};

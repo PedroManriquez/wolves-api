61eade0ec9818fcccb28625a3356804c0b7196f8
PedroManriquez <ptrmanriquez@gmail.com>
2018-01-19 16:48:40 -0300

    Enabled cors from breed resource routes

--- routes/breed.js
+++ routes/breed.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const breed = require('../app/controllers/breeds');
+const cors = require('cors');
 
 /* GET users listing. */
 router
-	.post('/breeds', breed.save)
-	.get('/breeds', breed.query)
-	.get('/breeds/:id', breed.get)
-	.put('/breeds/:id', breed.update)
-	.delete('/breeds/:id', breed.destroy);
+	.post('/breeds', cors(), breed.save)
+	.get('/breeds', cors(), breed.query)
+	.get('/breeds/:id', cors(), breed.get)
+	.put('/breeds/:id', cors(), breed.update)
+	.delete('/breeds/:id', cors(), breed.destroy);
 
 module.exports = router;

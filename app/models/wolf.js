0ee92d21ef1a95b398ab4cb5a0133fdbad04dcf6
PedroManriquez <ptrmanriquez@gmail.com>
2018-01-19 15:47:39 -0300

    Fix model wolf

--- app/models/wolf.js
+++ app/models/wolf.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false
     }
-  };
+  });
   wolf.associate = (models) => {
     wolf.belongsTo(models.breed, {
       foreignKey: 'breedId',

319dfcc73c640e709bbbd9ef77cc41e7d7b9c35b
PedroManriquez <ptrmanriquez@gmail.com>
2018-01-19 16:22:08 -0300

    Added delete route from breed resource

--- routes/breed.js
+++ routes/breed.js
@@ -6,6 +6,7 @@ const breed = require('../app/controllers/breeds');
 router
 	.post('/breeds', breed.create)
 	.get('/breeds', breed.index)
-	.get('/breeds/:id', breed.get);
+	.get('/breeds/:id', breed.get)
+	.delete('/breeds/:id', breed.destroy);
 
 module.exports = router;

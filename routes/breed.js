ca40643dab5ca3fca85eef3f4696af015511c2c6
PedroManriquez <ptrmanriquez@gmail.com>
2018-01-19 16:13:16 -0300

    Added route for get by id

--- routes/breed.js
+++ routes/breed.js
@@ -5,6 +5,7 @@ const breed = require('../app/controllers/breeds');
 /* GET users listing. */
 router
 	.post('/breeds', breed.create)
-	.get('/breeds', breed.index);
+	.get('/breeds', breed.index)
+	.get('/breeds/:id', breed.get);
 
 module.exports = router;

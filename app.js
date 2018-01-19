2eaa35eaee2bb1ae5f4990395430835e6ef74196
PedroManriquez <ptrmanriquez@gmail.com>
2018-01-19 14:07:54 -0300

    Fix import from routes directory in app.js

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+var api = require('./routes/index');
 var app = express();
 
 // view engine setup
@@ -22,8 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

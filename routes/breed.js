0c660cf791947beaebef84305d0ab58c03fb2bf5
PedroManriquez <ptrmanriquez@gmail.com>
2018-01-19 16:41:28 -0300

    Added .put methods in route from breed resource

--- routes/breed.js
+++ routes/breed.js
@@ -4,9 +4,10 @@ const breed = require('../app/controllers/breeds');
 
 /* GET users listing. */
 router
-	.post('/breeds', breed.create)
-	.get('/breeds', breed.index)
+	.post('/breeds', breed.save)
+	.get('/breeds', breed.query)
 	.get('/breeds/:id', breed.get)
+	.put('/breeds/:id', breed.update)
 	.delete('/breeds/:id', breed.destroy);
 
 module.exports = router;

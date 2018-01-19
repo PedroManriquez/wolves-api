4c8dc1195274d1ad6cffff1b81606fc54e3dccc8
PedroManriquez <ptrmanriquez@gmail.com>
2018-01-19 15:48:33 -0300

    Update source from index.js in routes for support api routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const api = express();
 // Import api routes
-
+const breed = require('./breed')
 /* GET home page. */
 const index = router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -10,6 +10,6 @@ const index = router.get('/', function(req, res, next) {
 
 api.use('/', index);
 /* API Router. */
-
+api.use('/api', breed);
 
 module.exports = api;
